5ec2e536b4e931822356b455eef0ed10b9edd99c
TheDevYago <yagocpm2020@gmail.com>
2026-05-22 16:47:40 -0300

    feat(relatorios): integra recepcao e download de arquivos PDF

--- src/app/core/services/relatorio.service.ts
+++ src/app/core/services/relatorio.service.ts
@@ -5,17 +5,18 @@ import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
+
 export class RelatorioService {
   private http = inject(HttpClient);
   private readonly API = `${environment.apiUrl}/relatorios`;
 
-  baixarRelatorio(tipoId: number, formato: string, filtros: any) {
+  baixarRelatorio(tipoId: string, formato: string, filtros: any) {
     let params = new HttpParams();
     if (filtros.inicio) params = params.set('dataInicio', filtros.inicio);
     if (filtros.fim) params = params.set('dataFim', filtros.fim);
     if (filtros.semestre) params = params.set('semestre', filtros.semestre);
     params = params.set('formato', formato);
 
-    return this.http.get(`${this.API}/${tipoId}`, { params, responseType: 'blob' });
+    return this.http.get(`${this.API}/download/${tipoId}`, { params, responseType: 'blob' });
   }
 }
